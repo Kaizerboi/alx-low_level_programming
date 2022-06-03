103a9d25582b0cd33c53988b65d73f2902355c03
Kaizerboi <naidooj15@gmail.com>
2022-06-03 13:06:22 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ int n, lastd;
 
 srand(time(0));
 n =  rand() - RAND_MAX / 2;
-lastd + n % 10;
+lastd = n % 10;
 
 if (lastd > 5)
 {
-ptintf("Last digit of %d is %d and is greater than 5|n", n, lastd);
+printf("Last digit of %d is %d and is greater than 5|n", n, lastd);
 }
 else if (lastd == 0)
 {

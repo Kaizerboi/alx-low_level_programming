bbe4676d3fd22723532d50466079eb1aefec048e
Kaizerboi <naidooj15@gmail.com>
2022-06-05 13:26:57 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - Prints a test according number
+* main - Prints a text according number
 *
 * Return: Always (Success)
 */
@@ -10,7 +10,7 @@ int main(void)
 {
 int n, lastd;
 srand(time(0));
-n =  rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 lastd = n % 10;
 if (lastd > 5)
 {
